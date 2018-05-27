55dfae369d57af33e332d4163f6197ac03c25054
xBlueCode <xbluecode@localhost.localdomain>
2018-05-27 10:03:48 +0300

    Initial commit

---
+++ src/main/java/xbc/tut/tutjavaspringbootmvcthymeleaf/TutJavaSpringBootMvcThymeleafApplication.java
@@ -0,0 +1,12 @@
+package xbc.tut.tutjavaspringbootmvcthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutJavaSpringBootMvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutJavaSpringBootMvcThymeleafApplication.class, args);
+	}
+}
